ba183b2478299d2326268a65b1c4fe5aafaeb5b8
vasmiya shirin <vasmiyak@gmail.com>
2025-11-30 19:02:35 +0300

    cors updated

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173", // dev
+    "https://movers-and-packers-frontend.vercel.app" // deployed frontend
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
